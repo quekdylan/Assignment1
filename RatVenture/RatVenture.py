380b9d7bc101816b89cfd3ce6154cc85e37de506
Dong Han Kua <s10189943@connect.np.edu.sg>
2020-12-30 17:36:25 +0800

    Added basic structure of code

--- RatVenture/RatVenture.py
+++ RatVenture/RatVenture.py
@@ -1,5 +1,41 @@
 from RatVenture_functions import *
+import sys
+
+#Default values
+v_name = "The Hero"
+v_damage = "2-4"
+v_defence = 1
+v_hp = 20
+v_location="0,0"
+v_day = 1
+
+#Display Main Menu 
+mainMenu()
+option = int(input("Enter your option: "))
+if(option == 1):
+    #newGame()
+    print("1")
 
 
+elif(option == 2):
+    #resumeGame()
+    print("2")
 
+elif(option == 3):
+    print("The game will now exit.")
+    quit()
+
+    
+while(True):
+    if(checkTown() == True):
+        townMenu()
+        choice = int(input("Enter your choice: "))
+    elif(checkEncounter() == True):
+        combatMenu()
+        choice = int(input("Enter your choice: "))
+    else:
+        outdoorMenu()
+        choice = int(input("Enter your choice: "))
+
+    
 
--- RatVenture/RatVenture_functions.py
+++ RatVenture/RatVenture_functions.py
@@ -1,4 +1,24 @@
 import sys
 
+def mainMenu():
+    #This function 
+    print("Welcome to Ratventure!")
+    print("----------------------")
+    print("1) New Game")
+    print("2) Resume Game")
+    print("3) Exit Game")
 
+def townMenu():
+    print("a")
 
+def combatMenu():
+    print("a")
+
+def outdoorMenu():
+    print("a")
+
+def checkTown():
+    #This function checks whether if the player is in town or not
+    #input: location
+    #returns: True if in town, False if not
+    print("a")

7ab1a1062321a065e1fcb795b4ffacc7c18f6159
Dong Han Kua <s10189943@connect.np.edu.sg>
2020-12-31 17:28:33 +0800

    Edited entity class

--- RatVenture/RatVenture_classes.py
+++ RatVenture/RatVenture_classes.py
@@ -11,8 +11,12 @@ class Entity:
         self.damage = damage
         self.defence = defence
 
-        
+
 
 # Example of how to initialize a entity    
 # player = Entity('The Hero', 20, "2-4", 1)
+# print(player.health)
+
+# Example of how to update variable in class
+# player.health = (player.health - 1)
 # print(player.health)

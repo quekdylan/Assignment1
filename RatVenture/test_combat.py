eb008e64dbb569ce48c701a75bec824345bebdce
Tsan Jia Jun <saphirestarx@gmail.com>
2021-01-13 14:03:54 +0800

    Update test_combat.py
    added commentsto conceptualize the test cases for Sprint 3

--- RatVenture/test_combat.py
+++ RatVenture/test_combat.py
@@ -2,9 +2,36 @@ import pytest
 from RatVenture_functions import *
 from RatVenture_classes import *
 
+##attack##
+
+#check initial player and enemy HP
+
+#check new HP
+
+#if <=0
+
+#check if game over screen?
+
+#if NOT 0
+
+#check enemy HP
+
+#if enemy HP <=0
+
+#return to map
+
+#else check if both player and enemy HP went down
+
+#check if it is using outdoor menu
 
 @pytest.mark.parameterize("hp",[(20)])
+
     def test_combat("hp, resultHP"):
         valueHP = fight(hp)
         assert valueHP > resultHP
     
+##run##
+
+#check if enemy instance HP went back to full
+
+#check if outdoor menu is selected

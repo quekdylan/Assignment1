0eab2e9548c46eaec444be037493d058e2636321
Tsan Jia Jun <saphirestarx@gmail.com>
2021-02-08 17:37:45 +0800

    Update test_checkorbs.py

--- RatVenture/test_checkorbs.py
+++ RatVenture/test_checkorbs.py
@@ -3,13 +3,13 @@ import pytest
 from RatVenture_functions import *
 
 def checkoriginalTown():
-    result = checkLocation("0,0")
-    assert result == "a town"
+    result = setOrbLocation("0,0")
+    assert result == null
 
 def checkorb():
-    result1 = checkLocation("3,1")
-    result2 = checkLocation("5,2")
-    result3 = checkLocation("1,3")
-    result4 = checkLocation("4,6")
+    result1 = setOrbLocation("3,1")
+    result2 = setOrbLocation("5,2")
+    result3 = setOrbLocation("1,3")
+    result4 = setOrbLocation("4,6")
 
-    assert result1 = "a town with orb" or result2 = "a town with orb" or result3 = "a town with orb" or result4 = "a town with orb"
+    assert result1 = orb_location or result2 = orb_location or result3 = orb_location or result4 = orb_location

745ae1da252d3ee8ad4bf1a37830e776f861b952
KuaDongHan <46645441+KuaDongHan@users.noreply.github.com>
2021-02-14 22:29:17 +0800

    Update RatVenture_classes.py

--- RatVenture/RatVenture_classes.py
+++ RatVenture/RatVenture_classes.py
@@ -1,22 +1,22 @@
 #This script contains the classes in RatVenture
 
 class Entity:
-    '''
+    """
     This is a class of entities e.g.(Player, Rat)
     Input: Name, Health, Damage, Defence
     Output:  Creates a object
-    '''
+    """
 
     def __init__(self, name, health, damage, defence):
-        '''
+        """
         name: init
         description: This function creates the entity object
-        params: {name:name, description: Name of entity, default: <defaultvalue or ''>}
+        params: {name:name, description: Name of entity}
         param name : Name of entity
-        self.health = Health of entity
+        self.health: Health of entity
         self.damage = Damage of entity
         self.defence = Defence of entity
-        '''
+        """
         self.name = name
         self.health = health
         self.damage = damage

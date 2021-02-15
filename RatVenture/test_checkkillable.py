4ed8ec682249ed60584685009a34c2ef40a127be
Tsan Jia Jun <saphirestarx@gmail.com>
2021-02-15 15:17:26 +0800

    updated killable test

--- RatVenture/test_checkkillable.py
+++ RatVenture/test_checkkillable.py
@@ -7,15 +7,13 @@ from RatVenture_classes import *
 #@pytest.mark.parametrize("player, enemy",[(p,r)])
 def test_checkkillable():
 #check initial player and enemy HP
-    player = Entity("The Hero", 20, "2-4", 1)
+    player = Entity("The Hero", 20, "7-9", 5, True)
     enemy = Entity('Rat King', 25, "8-12", 5)
 
-    #maybe a bool here for weather there is an orb?
-    porb = player.orb
     ehealth = enemy.health
     
-    player, enemy, status = attack(player, enemy)
-    assert player.orb = true
+    player, enemy, status = attack(player, enemy, player.orb)
+    assert player.orb == true
     assert enemy.health < ehealth
                    
 

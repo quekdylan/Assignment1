e32ba44419bcc44f232fa7ef88ffab36221f0eae
KuaDongHan <46645441+KuaDongHan@users.noreply.github.com>
2021-02-15 15:54:16 +0800

    Update RatVenture_classes.py

--- RatVenture/RatVenture_classes.py
+++ RatVenture/RatVenture_classes.py
@@ -2,13 +2,12 @@
 
 class Entity:
     """
+    @author Dong Han
+    
     This is a class of entities e.g.(Player, Rat, Rat King) \n
     Input: Name, Health, Damage, Defence, Orb=False \n
-    Output:  Creates a object
-    @author Dong Han
-    """
+    Output:  Creates a object \n \n
     
-    '''
     Example of how to initialize a entity    \n
     player = Entity('The Hero', 20, "2-4", 1) \n
     print(player.health) \n \n
@@ -16,7 +15,7 @@ class Entity:
     Example of how to update variable in class \n
     player.health = (player.health - 1) \n 
     print(player.health) \h
-    '''
+    """
     def __init__(self, name, health, damage, defence):
         """
         This function creates the entity object

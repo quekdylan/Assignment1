ea8a1964d55a51f60ec0c1f1936d9f35a184ccea
Tsan Jia Jun <saphirestarx@gmail.com>
2021-02-15 21:06:04 +0800

    Update test_checkstats.py

--- RatVenture/test_checkstats.py
+++ RatVenture/test_checkstats.py
@@ -7,9 +7,9 @@ def test_checkstats():
 #check initial player and enemy HP
     initialplayer = Entity("The Hero", 20, "2-4", 1)
     player = pickorb(initialplayer)
-    assert player.orb == true
-    assert initialplayer.damage == "7-9"
-    assert initialplayer.defence == "6"
+    assert player.orb == True
+    assert player.damage == "7-9"
+    assert player.defence == 6
     
 
     

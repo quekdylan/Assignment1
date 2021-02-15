b2c1b0a4995bbb4aa07cc859b5171a24fd74a22f
KuaDongHan <46645441+KuaDongHan@users.noreply.github.com>
2021-02-15 15:44:47 +0800

    Update RatVenture_classes.py

--- RatVenture/RatVenture_classes.py
+++ RatVenture/RatVenture_classes.py
@@ -2,19 +2,29 @@
 
 class Entity:
     """
-    This is a class of entities e.g.(Player, Rat) \n
-    Input: Name, Health, Damage, Defence \n
+    This is a class of entities e.g.(Player, Rat, Rat King) \n
+    Input: Name, Health, Damage, Defence, Orb=False \n
     Output:  Creates a object
     @author Dong Han
     """
+    
+    '''
+    Example of how to initialize a entity    \n
+    player = Entity('The Hero', 20, "2-4", 1) \n
+    print(player.health) \n \n
 
+    Example of how to update variable in class \n
+    player.health = (player.health - 1) \n 
+    print(player.health) \h
+    '''
     def __init__(self, name, health, damage, defence):
         """
         This function creates the entity object
-        @param name The name of the entity, test
+        @param name The name of the entity
         @param health The health of the entity
-        @param damage The damage of the entity, test
+        @param damage The damage of the entity
         @param defence The defence of the entity
+        @param orb True if the entity is holding the Orb
         @param self -
         """
         self.name = name
@@ -22,13 +32,4 @@ class Entity:
         self.damage = damage
         self.defence = defence
 
-
-
-# Example of how to initialize a entity    
-# player = Entity('The Hero', 20, "2-4", 1)
-# print(player.health)
-
-# Example of how to update variable in class
-# player.health = (player.health - 1)
-# print(player.health)
 
